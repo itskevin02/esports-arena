ec57015a6ceda944965db592643f6579ce63616c
kevin <kevi.reyesg@duocuc.cl>
2026-05-13 10:30:49 -0400

    init commit

---
+++ msvc-game/src/main/java/com/project/msvc_game/MsvcGameApplication.java
@@ -0,0 +1,13 @@
+package com.project.msvc_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcGameApplication.class, args);
+	}
+
+}
---
+++ msvc-game/src/test/java/com/project/msvc_game/MsvcGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.msvc_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
