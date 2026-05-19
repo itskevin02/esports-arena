018ce569a5151c3c458a7d70779bb821e1473b84
VA-Alumno <kevi.reyesg@duocuc.cl>
2026-05-19 10:42:10 -0400

    new modul

---
+++ msvc-tournament/src/main/java/com/project/msvc_tournament/MsvcTournamentApplication.java
@@ -0,0 +1,13 @@
+package com.project.msvc_tournament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcTournamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcTournamentApplication.class, args);
+	}
+
+}
