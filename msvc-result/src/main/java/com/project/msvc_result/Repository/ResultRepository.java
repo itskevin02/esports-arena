20518fb443e27e541afa17e554484a7c81bb216d
VA-Alumno <mae.vergara@duocuc.cl>
2026-05-19 11:34:59 -0400

    Result Terminado (Tener que revisar)

--- msvc-result/src/main/java/com/project/msvc_result/Repository/ResultRepository.java
+++ msvc-result/src/main/java/com/project/msvc_result/Repository/ResultRepository.java
@@ -1,4 +1,12 @@
 package com.project.msvc_result.Repository;
 
-public class ResultRepository {
+import com.project.msvc_result.Model.ResultModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface ResultRepository extends JpaRepository<ResultModel, Long> {
+    Optional<ResultModel> findByPartidaId(Long partidaId);
+    boolean existsByPartidaId(Long partidaId);
 }
