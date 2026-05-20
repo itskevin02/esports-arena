899637f5aa09eeee0cf91372ba4edd3bb0acc320
Martin <mae.vergara@duocuc.cl>
2026-05-19 20:11:35 -0400

    Team Terminado (Falta revisar Conexion)

--- msvc-team/src/main/java/com/project/msvc_team_service/Exception/TeamException.java
+++
@@ -1,7 +0,0 @@
-package com.project.msvc_team_service.Exception;
-
-public class TeamException extends RuntimeException {
-    public TeamException(String message) {
-        super(message);
-    }
-}
