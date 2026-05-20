e5cb0a2ddcaae885ecb9b40928b21a3a1623780d
itskevin02 <kevi.reyesg@duocuc.cl>
2026-05-20 17:07:35 -0400

    Delete msvc-result/src directory

--- msvc-result/src/main/java/com/project/msvc_result/Exception/ResultException.java
+++
@@ -1,7 +0,0 @@
-package com.project.msvc_result.Exception;
-
-public class ResultException extends RuntimeException {
-    public ResultException(String message) {
-        super(message);
-    }
-}
