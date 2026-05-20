775b35eac97a6ca55b20d4437817e9872d767048
itskevin02 <kevi.reyesg@duocuc.cl>
2026-05-20 17:07:55 -0400

    Delete msvc-prize/src directory

--- msvc-prize/src/main/java/com/project/msvc_prize/Exception/PrizeException.java
+++
@@ -1,7 +0,0 @@
-package com.project.msvc_prize_service.Exception;
-
-public class PrizeException extends RuntimeException {
-    public PrizeException(String message) {
-        super(message);
-    }
-}
