6ad761a99271a3a6eeca327b48bd5cdaa29eb5b2
itskevin02 <kevi.reyesg@duocuc.cl>
2026-05-20 17:06:36 -0400

    Delete msvc-registration/src directory

--- msvc-registration/src/main/java/com/project/msvc_registration/Exception/RegistrationException.java
+++
@@ -1,7 +0,0 @@
-package com.project.msvc_registration.Exception;
-
-public class RegistrationException extends RuntimeException {
-    public RegistrationException(String message) {
-        super(message);
-    }
-}
