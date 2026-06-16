a9f2c6c3c9b32e2f2d54e6fb57d740bca291a2b0
kevin <kevi.reyesg@duocuc.cl>
2026-06-16 16:05:49 -0400

    innit commit

---
+++ msvc-game/src/main/java/com/project/msvc_game/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.project.msvc_game.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Game Service API")
+                        .version("1.0")
+                        .description("Documentación del Game Service"));
+    }
+}
