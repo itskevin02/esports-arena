79d81b29a5501789a3544d3c0e76ccb0e931d17b
kevin <kevi.reyesg@duocuc.cl>
2026-06-19 11:02:00 -0400

    Swagger implementado

---
+++ msvc-user/src/main/java/com/project/msvc_user/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.project.msvc_user.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("eSports Arena Manager API")
+                        .version("1.0")
+                        .description("Documentación OpenAPI del microservicio de gestión de usuarios de eSports Arena Manager"));
+    }
+}
